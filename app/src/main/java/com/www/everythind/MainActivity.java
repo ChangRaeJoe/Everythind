e0ff9613f6742f8f414ed0be9887d38649a8123b
Joe <ckdfo1995@naver.com>
2019-02-26 16:51:22 +0900

    add /**/

--- app/src/main/java/com/www/everythind/MainActivity.java
+++ app/src/main/java/com/www/everythind/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
+
+/* Clone으로 github에서 가져오기 */
